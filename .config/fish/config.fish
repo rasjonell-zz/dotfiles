5630b908447a1726d441c21bfdedccd47ceb194e
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2020-03-27 21:20:18 +0400

    Add fish configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+set -x PATH "$HOME/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:/usr/local/opt/openssl@1.1/bin:/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
+
+set -U editor vim
+
+set -gx NVM_DIR $HOME/.nvm
