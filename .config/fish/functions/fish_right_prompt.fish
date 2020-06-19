bf4106b0242054cd315c987aaf4e377806ef9ef3
rasjonell <info.gurgen@gmail.com>
2020-06-19 20:22:30 +0400

    [Fish] Update fish stuff

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,39 +1,35 @@
 function fish_right_prompt
     set -l status_copy $status
     set -l status_code $status_copy
-    set -l status_color 555
-    set -l status_glyph
-    set -l duration_glyph
+
+    set -l color_normal (set_color normal)
+    set -l color_error (set_color $fish_color_error)
+    set -l color "$color_normal"
 
     switch "$status_copy"
-        case 0 "$__metro_status_last"
+        case 0 "$__mono_status_last"
             set status_code
     end
 
-    set -g __metro_status_last $status_copy
+    set -g __mono_status_last $status_copy
 
-    if test "$status_copy" -eq 0
-        set duration_glyph " "
-    else
-        set status_color red
-        set status_glyph │
+    if test "$status_copy" -ne 0
+        set color "$color_error"
     end
 
-    if test "$CMD_DURATION" -gt 250
-        if test ! -z "$status_code"
-            echo -sn (set_color $status_color) "($status_code)" (set_color normal)
-            set status_glyph ┃
-        end
-
-        set -l duration (echo $CMD_DURATION | humanize_duration)
-        echo -sn (set_color $status_color) " ($duration) $duration_glyph" (set_color normal)
-
-    else
-        if test ! -z "$status_code"
-            echo -sn (set_color $status_color) "$status_code " (set_color normal)
-            set status_glyph ┃
-        end
+    if set -l job_id (last_job_id)
+        echo -sn "$color%$job_id$color_normal "
     end
 
-    echo -sn (set_color $status_color) "$status_glyph" (set_color normal)
+    if test ! -z "$status_code"
+        echo -sn "$color$status_code$color_normal "
+    end
+
+    set -l duration "$CMD_DURATION$cmd_duration"
+    if test "$duration" -gt 250
+        echo -sn "$color"(echo "$duration" | humanize_duration)"$color_normal "
+    else
+        set -l venv_name (basename "$VIRTUAL_ENV")
+        echo -sn "$color$venv_name$color_normal "
+    end
 end
